519efa95465454484e6e269d68643ad1356b4e45
Ana Benites <anitabenites2014@gmail.com>
2018-07-05 10:53:06 +0200

    refactoring

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import Button from './Button';
 import Display from './Display';
+// import CalculatorDisplay from './CalculatorDisplay';
 // import OperationalButton from './OperationalButton';
+import updateDisplay from './CalculatorDisplay';
 
 export default class App extends React.Component {
   constructor(props) {
@@ -11,97 +13,17 @@ export default class App extends React.Component {
       operator: null,
       operand2: null
     }
-    this.updateDisplay = this.updateDisplay.bind(this);
+    this.updateDisplayApp = this.updateDisplayApp.bind(this);
   }
 
-  updateDisplay(event) {
-    // TODO: split this function into smaller parts, for example extract functionalities into separate functions
+   updateDisplayApp(event){
+     // console.log("this is the event",event);
+     // console.log("This is CalculatorDisplay", CalculatorDisplay);
+      updateDisplay(event);
 
-    var inputNumber = event.target.innerHTML; // whatever I click in the calculator will be saved in inputNumber
-    //event.target.innerHTML gives me a string.
-    //fetching = get something
-    if(parseInt(inputNumber)>= 0 && parseInt(inputNumber)<= 9){
-      if(!this.state.operand1){ // it gives true/false
-        this.setState(
-          {operand1: parseInt(event.target.innerHTML)}, // state object as a first parameter
-          function() {console.log(this.state)} // is a callback function, once the first object is completed the other ones works
-        )
-      }
-      else if(this.state.operand1 && !this.state.operator) {
-        console.log(this.state.operand1);
-        console.log(inputNumber);
-        // TODO calculate new state and then call setState() once in the end
-        // TODO use local variables to store intermediate values
-        this.setState({operand1: parseInt(this.state.operand1*10+parseInt(event.target.innerHTML))}, function(){console.log(this.state)})
-      }
-      else if(this.state.operand1 && this.state.operator && !this.state.operand2) {
-        this.setState({operand2: parseInt(event.target.innerHTML)}, function(){console.log(this.state)})
-      }
-      else if(this.state.operand1 && this.state.operator && this.state.operand2){
-        this.setState({operand2: parseInt(this.state.operand2*10+parseInt(event.target.innerHTML))}, function(){console.log(this.state)})
-      }
-    }
-  else if(inputNumber==='C'){
-    this.setState({
-      operand1:null,
-      operator: null,
-      operand2: null
-    }, function() {console.log(this.state)})
   }
-  else if(inputNumber=='=') {
-    // TODO extract this into a new function
-    if(this.state.operand1 && this.state.operand2 && this.state.operator){
-      if(this.state.operator == "+"){
-        // set state once after the conditions
-        this.setState({
-          operand1: this.state.operand1 + this.state.operand2,
-          operator: null,
-          operand2: null
-        }, function(){console.log(this.state)})
-      }
-      else if(this.state.operator=="-"){
-        this.setState({
-          operand1: this.state.operand1 - this.state.operand2,
-          operator: null,
-          operand2: null
-        }, function(){console.log(this.state)})
-      }
-      else if(this.state.operator=="*"){
-        this.setState({
-          operand1:this.state.operand1*this.state.operand2,
-          operator:null,
-          operand2:null
-        }, function(){console.log(this.state)})
-      }
-      else if(this.state.operator=="/"){
-        this.setState({
-          operand1: this.state.operand1/this.state.operand2,
-          operator: null,
-          operand2: null
-        }, function() {console.log(this.state)})
-      }
-      else if(this.state.operator=="%"){
-        this.setState({
-          operand1: this.state.operand1%this.state.operand2,
-          operator: null,
-          operand2: null
-        }, function(){console.log(this.state)})
-      }
-    }
-  }
-  else if(inputNumber == '+/-'){
-    if(this.state.operand1 && !this.state.operator && !this.state.operand2){
-      if(this.state.operand1){
-        this.setState({operand1: -this.state.operand1}, function(){console.log(this.state)})
-      }
-    }
-  }
-  else if(inputNumber=='+' || inputNumber == '-' || inputNumber == '*' || inputNumber == '/' || inputNumber == '%') {
-    if(this.state.operand1){
-      this.setState({operator: event.target.innerHTML}, function(){console.log(this.state)})
-    }
-  }
-}
+
+
 
   render(){
     // TODO: Introduce ButtonPanel component to store Button components
@@ -109,7 +31,7 @@ export default class App extends React.Component {
     return(
       <div className="App">
         <Display inputs={this.state}/>
-        <Button triggerDisplayUpdate = {this.updateDisplay}/>
+        <Button triggerDisplayUpdate = {this.updateDisplayApp}/>
       </div>
     );
   }
